80410227439a96a008b5a7fab241d3743cdaf15f
RamadhanZaki <amimi1040@gmail.com>
2025-05-21 22:01:28 +0700

    Updates

---
+++ js/about.e560d044.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkjogja_inside"]=self["webpackChunkjogja_inside"]||[]).push([[594],{1417:function(n,s,t){t.r(s),t.d(s,{default:function(){return r}});var u=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,s=n._self._c;return s("div",{staticClass:"about"},[s("h1",[n._v("This is an about page")])])}],i=t(1656),a={},l=(0,i.A)(a,u,e,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about.e560d044.js.map
