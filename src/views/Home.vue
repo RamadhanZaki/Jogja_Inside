327eb1acecc53f57505b2ed79bc846019b9ac225
kamill21 <kamilrizqullah21@students.amikom.ac.id>
2025-05-22 09:37:53 +0700

    Update_4_Kamill_IntroText

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Header/>
+    <IntroText/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from '@/components/Home/Header.vue'
+import IntroText from '@/components/Home/IntroText.vue'
 
 export default {
   name: 'HomePage',
   components: {
     Header,
+    IntroText,
   }
 }
 </script>
