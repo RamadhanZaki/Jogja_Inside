cd0b0de4eb3a7a2f0a64b759ff05c776aa4b7658
DebiSaputra26 <debisaputra945@gmail.com>
2025-08-02 18:08:59 +0700

    Update Debi 10

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,37 +1,41 @@
 <template>
   <div class="home">
-    <Navbar/>
-    <Header/>
-    <IntroText/>
-    <TopThree />
-    <HiddenGems/>
-    <TopDestinations/>
-    <WhyChooseUs/>
-    <Footer/>
+    <Navbar />
+    <Header />
+    <RunningText />
+    <ExploreDestination/>
+    <TopDestinations />
+    <WhyChooseUs />
+    <HiddenGems />
+    <BannerBottom/>
+    <Footer />
   </div>
 </template>
+
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/Navbar.vue'
 import Header from '@/components/Home/Header.vue'
-import IntroText from '@/components/Home/IntroText.vue'
-import TopDestinations from '@/components/TopDestination.vue'
+import RunningText from '@/components/Home/RunningText.vue'
+import ExploreDestination from '@/components/Home/ExploreDestination.vue'
+import TopDestinations from '@/components/Home/TopDestination.vue'
 import HiddenGems from '@/components/Home/HiddenGems.vue'
-import TopThree from '@/components/TopThree.vue' 
 import Footer from '@/components/Footer.vue'
 import WhyChooseUs from '@/components/Home/WhyChooseUs.vue'
+import BannerBottom from '@/components/Home/BannerBottom.vue'
 
 export default {
   name: 'HomePage',
   components: {
     Navbar,
     Header,
-    IntroText,
-    TopThree,
+    RunningText,
+    ExploreDestination,
     TopDestinations,
     HiddenGems,
     WhyChooseUs,
+    BannerBottom,
     Footer,
     
   }
