c2a5b588dbefc1b3c637d49809a0f9d2ea803694
Dito441 <todidito130@gmail.com>
2025-08-02 16:51:55 +0700

    Update Dito 8

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+
+// ✅ Atur baseURL dari environment dengan benar
+axios.defaults.baseURL = process.env.VUE_APP_API_BASE
+
+// ✅ Inject axios agar bisa dipakai via this.$axios
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
